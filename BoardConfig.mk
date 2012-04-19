179c36dd144c813212a71ed646b970c9054b929c
travp624 <stygianstudiosdevs@gmail.com>
2012-04-19 02:09:53 -0500

    update modules from .621 build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,7 @@ BOARD_OVERLAY_BASED_CAMERA_HAL := true
 
 # Recovery
 TARGET_PREBUILT_RECOVERY_KERNEL := device/moto/shadow/kernel
-BOARD_MKE2FS := device/moto/shadow/releaseutils/mke2fs
+BOARD_MKE2FS := device/moto/omap34com/releaseutils/mke2fs
 
 TARGET_PROVIDES_INIT_RC := true
 
